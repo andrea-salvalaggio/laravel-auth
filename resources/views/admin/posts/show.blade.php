aea199434d1114ada830a2d7a031e65bcc122e8d
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-09-19 17:33:15 +0200

    Create: show page with data

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -3,8 +3,16 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-12">
-                ciao
+            <div class="col-12 my-5">
+                <div class="card mb-3">
+                    <img src="{{ $post->post_image }}" class="card-img-top" alt="{{ $post->title }}">
+                    <div class="card-body">
+                        <h2>{{ $post->post_title }}</h2>
+                        <p class="card-text">{{ $post->post_content }}</p>
+                        <span class="badge text-white bg-dark p-2">{{ $post->post_author }}</span>
+                        <span class="badge text-white bg-dark p-2">{{ $post->post_date }}</span>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
